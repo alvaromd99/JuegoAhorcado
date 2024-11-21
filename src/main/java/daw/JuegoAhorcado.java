05e4496ad8f4b366ab73079805193b11565f4fa4
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-11-21 09:20:29 +0100

    add hide word, its incompleted

--- src/main/java/daw/JuegoAhorcado.java
+++ src/main/java/daw/JuegoAhorcado.java
@@ -24,23 +24,41 @@ public class JuegoAhorcado {
                       """;
 
         int opcion = 0;
+        String wordToGuess = "";
+        String userWord = "";
+        char vowel = Character.MIN_VALUE;
 
         do {
             opcion = getOption(menu);
+            
+            if (opcion == Integer.MIN_VALUE) break;
+            
+            switch (opcion) {
+                case 1 ->
+                    System.out.println("Op 1");
+                case 2 -> {
+                    wordToGuess = Palabras.getRandomWord();
+                    vowel = getFirstVowel(wordToGuess);
+                }
+                case 3 ->
+                    System.out.println("Hasta luego");
+                default ->
+                    JOptionPane.showMessageDialog(null, "Opcion invalida");
+            }
         } while (opcion != 3 && opcion != Integer.MIN_VALUE);
     }
-    
+
     /**
      * Pide al usuario, usando joption, una opcion mostrando un menu
-     * 
+     *
      * @param menu el menu que vamos a mostrar
-     * @return  el numero entero que representa la opcion que ha elegido el 
-     *          usuario
+     * @return el numero entero que representa la opcion que ha elegido el
+     * usuario
      */
     public static int getOption(String menu) {
         String tmp = "";
         int op = 0;
-        
+
         tmp = JOptionPane.showInputDialog(menu);
 
         try {
@@ -55,5 +73,45 @@ public class JuegoAhorcado {
                     + " opcion valida.");
         }
         return op;
+    }
+    
+    /**
+     * Devuelve la primera vocal que hay en un string
+     * 
+     * @param str el string donde vamos a buscar la vocal
+     * @return  la primera vocal que hemos encontrado en la palabra o el valor
+     *          minimo de Character si no hemos encontrado vocales
+     */
+    public static char getFirstVowel(String str) {
+        char ch = Character.MIN_VALUE;
+        
+        for (int i = 0; i < str.length(); i++) {
+            if("AEIOUaeiou".indexOf(str.charAt(i)) != -1) {
+                ch = str.charAt(i);
+            }
+        }
+        return ch;
+    }
+    
+    public static String hideWordToGuess(String str, char vowel) {
+        String res = "";
+        // valueof -> char[] to String
+        for (int i = 0; i < str.length(); i++) {
+            
+        }
+        return res;
+    }
+    
+    
+    /**
+     * Devuelve un string que es la palabra que hemos pasado pero poniendo un
+     * espacio entre cada uno de los caracteres
+     * hola -> h o l a
+     * 
+     * @param str el string que queremos separar
+     * @return el string con espacios entre cada uno de los caracteres
+     */
+    public static String showStringWithSpaces(String str) {
+        return String.join(" ", str.split(""));
     }
 }
